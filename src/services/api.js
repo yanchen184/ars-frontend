dfeb222c5006b7bfd933b515f0c08b06ae0378c9
yanchen184 <62279165+yanchen184@users.noreply.github.com>
2025-04-28 14:08:28 +0800

    將API baseURL修改為 http://192.168.1.165:8080/hktv_ars

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Base axios instance for all API calls
 const api = axios.create({
-  baseURL: 'http://192.168.1.165:8080',
+  baseURL: 'http://192.168.1.165:8080/hktv_ars',
   headers: {
     'Content-Type': 'application/json',
   },
